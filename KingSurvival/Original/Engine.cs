39484010a76d4b413dfcf55902386f8635465402
Pavel Hristov <pavelh@abv.bg>
2014-06-01 15:40:09 +0300

    Added new classes Engine.cs, Game.cs, GamePiece.cs, King.cs, Pawn.cs and interfaces IRenderable.cs and IRenderer.cs.

---
+++ KingSurvival/Original/Engine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Original
+{
+    class Engine
+    {
+        private IRenderer renderer;
+
+        public Engine(IRenderer renderer)
+        {
+            
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+               
+
+            }
+        }
+    }
+}
+
