0767cd9e3b870dca3553cc18fd8b3ea60581fb31
phristov <pavelh@abv.bg>
2014-07-05 19:00:32 +0300

    Moved IRenderable.cs and IRenderer.cs to correct folder.

--- KingSurvival/KingSurvival/ConsoleRenderer.cs
+++ KingSurvival/KingSurvival/ConsoleRenderer.cs
@@ -1,54 +1,25 @@
-﻿namespace KingSurvival
+﻿namespace KingSurvivalGame
 {
     using System;
 
-    /// <summary>
-    /// Renderer that uses the System.Console class to draw on the console
-    /// </summary>
-    public class ConsoleRenderer : IRenderer
+    public class ConsoleRenderer : RendererBase // decorator design pattern concrete component
     {
-        // Singleton design pattern implementation
-        private static ConsoleRenderer instance;
-
-        private ConsoleRenderer()
+        public override void Render(char[,] objectToRender, int offsetX, int offsetY)
         {
-        }
-
-        /// <summary>
-        /// Returns the only instance of this class
-        /// </summary>
-        public static ConsoleRenderer Instance
-        {
-            get
+            for (int row = 0; row < objectToRender.GetLength(0); row++)
             {
-                if (instance == null)
-                {
-                    instance = new ConsoleRenderer();
-                }
+                Console.SetCursorPosition(offsetX, offsetY + row);
 
-                return instance;
+                for (int col = 0; col < objectToRender.GetLength(1); col++)
+                {
+                    Console.Write(objectToRender[row,col]);
+                }
             }
         }
 
-        /// <summary>
-        /// Draws an object on the System.Console by givven an matrix of char elements and initial top left position
-        /// </summary>
-        /// <param name="image">2 Dimensional array representing the image to render</param>
-        /// <param name="left">The leftmost position from wich to begin the drawing</param>
-        /// <param name="top">the topmost position from wich to begin the drawing</param>
-        public void Render(char[,] image, int left, int top)
+        public override void ClearScreen()
         {
-            for (int row = 0; row < image.GetLength(0); row++)
-            {
-                // on every row the cursor is put back to the initial left position
-                // for each of the rows the cursor is moved down once
-                Console.SetCursorPosition(left, top - row);
-
-                for (int col = 0; col < image.GetLength(1); col++)
-                {
-                    Console.Write(image[row,col]);
-                }
-            }
+            Console.Clear();
         }
     }
 }
