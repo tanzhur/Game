dabe521df5ff2fa5740d2b45cdd4680b8af42b06
fast4y <dzhenko@gmail.com>
2014-07-08 15:18:03 +0300

    Added PawnsAndKingsFactory class implementing IGamePieceFactory for the abstract factory creational design pattern. Returns King as the player 1 piece and Pawn for the player 2 piece

---
+++ KingSurvival/KingSurvival/Interfaces/IGamePieceFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace KingSurvivalGame.Interfaces
+{
+    public interface IGamePieceFactory // abstract factory design pattern
+    {
+        IPiece CreatePlayer1Piece();
+
+        IPiece CreatePlayer2Piece();
+    }
+}
---
+++ KingSurvival/KingSurvival/PawnsAndKingsFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace KingSurvivalGame
+{
+    using KingSurvivalGame.Interfaces;
+
+    public class PawnsAndKingsFactory : IGamePieceFactory // abstract factory design pattern
+    {
+        public IPiece CreatePlayer1Piece()
+        {
+            return new PieceKing();
+        }
+
+        public IPiece CreatePlayer2Piece()
+        {
+            return new PiecePawn();
+        }
+    }
+}
