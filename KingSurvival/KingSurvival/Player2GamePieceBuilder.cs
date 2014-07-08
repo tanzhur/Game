fa182d7e56aa6b43b02a030b86bb4911a3c4e2e5
fast4y <dzhenko@gmail.com>
2014-07-08 15:33:15 +0300

    Added Player2GamePieceBuilder class - inheriting from PlayersGamePieceBuilder this is the second game piece builder (player 2) for the builder creational design pattern

---
+++ KingSurvival/KingSurvival/Player2GamePieceBuilder.cs
@@ -0,0 +1,48 @@
+﻿namespace KingSurvival
+{
+    using Interfaces;
+
+    public class Player2GamePieceBuilder : PlayersGamePieceBuilder
+    // builder design pattern - knows how and which properties to set on player one pieces
+    {
+        private readonly char[] PieceID = new char[] { GameConstants.PawnAName, GameConstants.PawnBName, 
+                                              GameConstants.PawnCName, GameConstants.PawnDName };
+
+        private readonly int[] startPositionsX = new int[] {GameConstants.PawnAStartCol, GameConstants.PawnBStartCol, 
+                                                  GameConstants.PawnCStartCol, GameConstants.PawnDStartCol};
+
+        private readonly int[] startPositionsY = new int[] {GameConstants.PawnAStartRow, GameConstants.PawnBStartRow, 
+                                                  GameConstants.PawnCStartRow, GameConstants.PawnDStartRow};
+
+        private int pieceIndex;
+
+        public Player2GamePieceBuilder(IGamePieceFactory factory)
+            : base(factory)
+        {
+            this.pieceIndex = 0;
+        }
+
+        public override void CreatePiece()
+        {
+            this.Piece = this.Factory.CreatePlayer2Piece();
+        }
+
+        public override void SetPieceID()
+        {
+            this.Piece.ID = this.PieceID[this.pieceIndex];
+        }
+
+        public override void SetPieceCoordinates()
+        {
+            this.Piece.Coordinates = new Coordinates(this.startPositionsX[this.pieceIndex],
+                                                     this.startPositionsY[this.pieceIndex]);
+        }
+
+        public override IPiece GetPiece()
+        {
+            this.pieceIndex++; // getting ready for the next piece only after we take this one
+            // protect the data here - if index out of range ??
+            return this.Piece;
+        }
+    }
+}
