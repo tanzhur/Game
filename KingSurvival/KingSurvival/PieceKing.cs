bb6e5bc54f637cc98b17995468ffe5c7c6bbd903
fast4y <dzhenko@gmail.com>
2014-07-08 11:25:08 +0300

    Added PieceKing class implementing the IPiece interface and inheriting Piece. All alowed moves are 4.

---
+++ KingSurvival/KingSurvival/PieceKing.cs
@@ -0,0 +1,55 @@
+﻿namespace KingSurvivalGame
+{
+    using KingSurvivalGame.Enums;
+    using KingSurvivalGame.Interfaces;
+
+    public class PieceKing : Piece, IPiece
+    {
+        public override bool IsValidCommand(ICommand command)
+        {
+            if (this.ID != command.TargetID)
+            {
+                return false;
+            }
+
+            if (command.Move == Moves.DownLeft || command.Move == Moves.DownRight
+                 || command.Move == Moves.UpLeft || command.Move == Moves.UpRight)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override ICoordinates GetNewCoordinates(Moves move)
+        {
+            if (move == Moves.UpLeft)
+            {
+                return new Coordinates(this.Coordinates.X - 1, this.Coordinates.Y - 1);
+            }
+            else if (move == Moves.UpRight)
+            {
+                return new Coordinates(this.Coordinates.X + 1, this.Coordinates.Y - 1);
+            }
+            else if (move == Moves.DownLeft)
+            {
+                return new Coordinates(this.Coordinates.X - 1, this.Coordinates.Y + 1);
+            }
+            else if (move == Moves.DownRight)
+            {
+                return new Coordinates(this.Coordinates.X + 1, this.Coordinates.Y + 1);
+            }
+
+            return null;
+        }
+
+        public override void Move(ICoordinates coordinates)
+        {
+            this.Coordinates = new Coordinates(coordinates.X, coordinates.Y);
+
+            this.NotifyObservers();
+        }
+    }
+}
