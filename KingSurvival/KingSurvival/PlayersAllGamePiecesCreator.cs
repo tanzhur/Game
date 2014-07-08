8fad27df090048d1337dfaa5c39769a3cebc9197
fast4y <dzhenko@gmail.com>
2014-07-08 15:46:21 +0300

    Added PlayersAllGamePiecesCreator class. Works as a facade to create all game pieces in the game. Exposes a single method CreateGamePieces. Returns a List of Lists of IPieces. Each List is for the players (1 2 3 etc..) Uses Abstract Factory (PawnsAndKingsFactory) and Builders (player1PieceBuilder and player2PieceBuilder commanded by the PlayerGamePieceDirector class). Creates 1 King and 4 Pawns as Player 1 piece and Player 2 pieces.

---
+++ KingSurvival/KingSurvival/PlayersAllGamePiecesCreator.cs
@@ -0,0 +1,52 @@
+﻿namespace KingSurvival
+{
+    using System.Collections.Generic;
+
+    using Interfaces;
+
+    public class PlayersAllGamePiecesCreator //facade design pattern //singleton ?
+    {
+        public PlayersAllGamePiecesCreator()
+        {
+        }
+
+        public IList<IList<IPiece>> CreateGamePieces()
+        {
+            // main factory
+            var factory = new PawnsAndKingsFactory();
+
+            // player 1
+            var player1PieceBuilder = new Player1GamePieceBuilder(factory);
+            var player1PieceCreator = new PlayerGamePieceDirector(player1PieceBuilder);
+
+            var player1GamePieces = new List<IPiece>();
+
+            // 1 piece
+            player1PieceCreator.CreateGamePiece();
+            player1GamePieces.Add(player1PieceCreator.GetGamePiece());
+
+            // player 2
+            var player2PieceBuilder = new Player2GamePieceBuilder(factory);
+            var player2PieceCreator = new PlayerGamePieceDirector(player2PieceBuilder);
+
+            var player2GamePieces = new List<IPiece>();
+
+            // 4 pieces
+            player2PieceCreator.CreateGamePiece();
+            player2GamePieces.Add(player2PieceCreator.GetGamePiece());
+            player2PieceCreator.CreateGamePiece();
+            player2GamePieces.Add(player2PieceCreator.GetGamePiece());
+            player2PieceCreator.CreateGamePiece();
+            player2GamePieces.Add(player2PieceCreator.GetGamePiece());
+            player2PieceCreator.CreateGamePiece();
+            player2GamePieces.Add(player2PieceCreator.GetGamePiece());
+
+            var listToReturn = new List<IList<IPiece>>();
+
+            listToReturn.Add(player1GamePieces);
+            listToReturn.Add(player2GamePieces);
+
+            return listToReturn;
+        }
+    }
+}
