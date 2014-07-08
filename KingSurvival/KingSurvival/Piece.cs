a5dbab32fb3fc28fb985e747cee5232d0b063abf
fast4y <dzhenko@gmail.com>
2014-07-08 11:23:45 +0300

    Added abstract class Piece - to serve as a base for all Pieces for this game. Added comments for design patterns concerning this class and all of it's inheritants.

---
+++ KingSurvival/KingSurvival/Piece.cs
@@ -0,0 +1,80 @@
+﻿namespace KingSurvivalGame
+{
+    using System;
+    using System.Collections.Generic;
+
+    using KingSurvivalGame.Enums;
+    using KingSurvivalGame.Interfaces;
+
+    public abstract class Piece : IPiece
+    {
+        private char id;
+        private ICoordinates coordinates;
+
+        protected readonly IList<IGamePieceObserver> observers; // needed by observer patern (each figure holds a list of observers that
+        // it needs to notify everytime it moves
+
+        public Piece()
+        {
+            // must remain an empty ctor for builder design pattern!
+            this.observers = new List<IGamePieceObserver>();
+        }
+
+        public char ID
+        {
+            get
+            {
+                return this.id;
+            }
+
+            set
+            {
+                // TODO: CheckData
+                this.id = value;
+            }
+        }
+
+        public ICoordinates Coordinates
+        {
+            get
+            {
+                return this.coordinates;
+            }
+
+            set
+            {
+                // TODO: CheckData
+                this.coordinates = value;
+            }
+        }
+        public abstract bool IsValidCommand(ICommand command);
+
+        public abstract ICoordinates GetNewCoordinates(Moves move);
+
+        public abstract void Move(ICoordinates coordinates); //have to call Notify Method Here on every move
+
+        public void SubscribeToGamePieceObserver(IGamePieceObserver observer)
+        {
+            if (!this.observers.Contains(observer))
+            {
+                this.observers.Add(observer);
+            }
+        }
+
+        public void UnSubscribeFromGamePieceObserver(IGamePieceObserver observer)
+        {
+            if (this.observers.Contains(observer))
+            {
+                this.observers.Remove(observer);
+            }
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in this.observers)
+            {
+                observer.Notify(this.ID, this.Coordinates);
+            }
+        }
+    }
+}
