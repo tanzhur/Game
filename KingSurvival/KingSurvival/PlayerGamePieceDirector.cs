9d252b4dc5cb409ba3c43af031cefd9d486dc7c0
fast4y <dzhenko@gmail.com>
2014-07-08 15:35:59 +0300

    Added PlayerGamePieceDirector class - this is the director for the builder creational design pattern. Uses a builder to create the piece by dictating the steps by which the piece is created (creation, setting of the ID, setting of the coordinates)

---
+++ KingSurvival/KingSurvival/PlayerGamePieceDirector.cs
@@ -0,0 +1,26 @@
+﻿namespace KingSurvival
+{
+    using Interfaces;
+
+    public class PlayerGamePieceDirector // builder design pattern this is the director
+    {
+        private readonly PlayersGamePieceBuilder builder;
+
+        public PlayerGamePieceDirector(PlayersGamePieceBuilder builder)
+        {
+            this.builder = builder; // check data
+        }
+
+        public void CreateGamePiece()
+        {
+            this.builder.CreatePiece();
+            this.builder.SetPieceID();
+            this.builder.SetPieceCoordinates();
+        }
+
+        public IPiece GetGamePiece()
+        {
+            return this.builder.GetPiece();
+        }
+    }
+}
