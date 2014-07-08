a612792649352d99fef403bb9ec00202174b5244
fast4y <dzhenko@gmail.com>
2014-07-08 11:22:30 +0300

    Added IPiece interface

---
+++ KingSurvival/KingSurvival/Interfaces/IPiece.cs
@@ -0,0 +1,24 @@
+﻿namespace KingSurvivalGame.Interfaces
+{
+    using KingSurvivalGame.Enums;
+
+    public interface IPiece
+    {
+        char ID { get; set; }
+
+        ICoordinates Coordinates { get; set; }
+
+        bool IsValidCommand(ICommand command);
+
+        ICoordinates GetNewCoordinates(Moves move);
+
+        void Move(ICoordinates newCoordinates);
+
+        // these 3 can be into a seperate interface (IObservableGamePiece)
+        void SubscribeToGamePieceObserver(IGamePieceObserver observer);
+
+        void UnSubscribeFromGamePieceObserver(IGamePieceObserver observer);
+
+        void NotifyObservers();
+    }
+}
