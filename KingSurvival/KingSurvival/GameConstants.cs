4802e65503647544b16d4e710fe25c056b59efa8
hristo-iliev <hristo.iliev1988@gmail.com>
2014-07-08 12:38:13 +0300

    Adding Game Constants

---
+++ KingSurvival/KingSurvival/GameConstants.cs
@@ -0,0 +1,31 @@
+﻿namespace KingSurvivalGame
+{
+    public static class GameConstants
+    {
+        public const int PawnAStartRow = 0;
+        public const int PawnAStartCol = 0;
+        public const char PawnAName = 'A';
+
+        public const int PawnBStartRow = 0;
+        public const int PawnBStartCol = 2;
+        public const char PawnBName = 'B';
+
+        public const int PawnCStartRow = 0;
+        public const int PawnCStartCol = 4;
+        public const char PawnCName = 'C';
+
+        public const int PawnDStartRow = 0;
+        public const int PawnDStartCol = 6;
+        public const char PawnDName = 'D';
+
+        public const int KingStartRow = 7;
+        public const int KingStartCol = 3;
+        public const char KingName = 'K';
+
+        public const int MessageShowTimeMilliseconds = 2000;
+
+        public const string IllegalMove = "Illegal move!";
+        public const string player1Turn = "King's turn: ";
+        public const string player2Turn = "Pawns' turn: ";
+    }
+}
