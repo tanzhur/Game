98923bd72552b79270254731dc7777577816ce54
fast4y <dzhenko@gmail.com>
2014-07-08 15:29:52 +0300

    Added abstract class PlayersGamePieceBuilder - this is the base for the builder design pattern

---
+++ KingSurvival/KingSurvival/PlayersGamePieceBuilder.cs
@@ -0,0 +1,24 @@
+﻿namespace KingSurvival
+{
+    using Interfaces;
+
+    public abstract class PlayersGamePieceBuilder // builder design pattern - this is the base
+    {
+        protected readonly IGamePieceFactory Factory;
+
+        public PlayersGamePieceBuilder(IGamePieceFactory factory)
+        {
+            this.Factory = factory; // check data
+        }
+
+        protected IPiece Piece;
+
+        public abstract void CreatePiece();
+
+        public abstract void SetPieceID();
+
+        public abstract void SetPieceCoordinates();
+
+        public abstract IPiece GetPiece();
+    }
+}
