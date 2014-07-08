29f703ab4f0b355a1f9bc51da4992da2455648cb
fast4y <dzhenko@gmail.com>
2014-07-08 11:25:58 +0300

    Added PiecePawn class implementing the IPiece interface and inheriting Piece. All alowed moves are 2.

---
+++ KingSurvival/KingSurvival/PiecePawn.cs
@@ -0,0 +1,46 @@
+﻿namespace KingSurvivalGame
+{
+    using KingSurvivalGame.Enums;
+    using KingSurvivalGame.Interfaces;
+
+    public class PiecePawn : Piece, IPiece
+    {
+        public override bool IsValidCommand(ICommand command)
+        {
+            if (this.ID != command.TargetID)
+            {
+                return false;
+            }
+
+            if (command.Move == Moves.DownLeft || command.Move == Moves.DownRight)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override ICoordinates GetNewCoordinates(Moves move)
+        {
+            if (move == Moves.DownLeft)
+            {
+                return new Coordinates(this.Coordinates.X - 1, this.Coordinates.Y + 1);
+            }
+            else if (move == Moves.DownRight)
+            {
+                return new Coordinates(this.Coordinates.X + 1, this.Coordinates.Y + 1);
+            }
+
+            return null;
+        }
+
+        public override void Move(ICoordinates coordinates)
+        {
+            this.Coordinates = new Coordinates(coordinates.X, coordinates.Y);
+
+            this.NotifyObservers();
+        }
+    }
+}
