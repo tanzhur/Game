9894add5e17d7c8cf9e87a3d71086b6a189a391a
phristov <pavelh@abv.bg>
2014-07-11 03:58:40 +0300

    Renamed file to RendererPlain.cs

--- KingSurvival/KingSurvival/RendererPlain.cs
+++ KingSurvival/KingSurvival/RendererPlain.cs
@@ -1,6 +1,6 @@
 ﻿namespace KingSurvival
 {
-    public abstract class RendererBase // decorator design pattern component base
+    public abstract class RendererPlain // decorator design pattern component base
     {
         public abstract void Render(char[,] objectToRender, int initialLeft, int initialTop);
 
