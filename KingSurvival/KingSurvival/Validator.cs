9e231b22eb2d921302c8dd7b3a8ec49efcc2390c
phristov <pavelh@abv.bg>
2014-07-11 05:20:01 +0300

    Added Logger.

--- KingSurvival/KingSurvival/Validator.cs
+++ KingSurvival/KingSurvival/Validator.cs
@@ -1,19 +1,33 @@
 ﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace KingSurvival
 {
     using System;
     public static class Validator
     {
-        public static void CheckValueIsNull(object value, string propertyName)
+        public static void CheckValueIsNull(object value, string propertyName,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
         {
             if (value == null)
             {
-                throw new ArgumentNullException(propertyName + " can not be null.");
+                try
+                {
+                    throw new ArgumentNullException(propertyName + " can not be null.");
+                }
+                catch (ArgumentNullException argumentNullException)
+                {
+                   Logger.LogException(argumentNullException, sourceFilePath, memberName, sourceLineNumber);
+                }
             }
         }
 
-        public static void CheckStringNullOrEmpty(string value, string propertyName)
+        public static void CheckStringNullOrEmpty(string value, string propertyName,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
         {
             if (string.IsNullOrEmpty(value))
             {
@@ -21,7 +35,10 @@ namespace KingSurvival
             }
         }
 
-        public static void CheckStringNullOrWhiteSpace(string value, string propertyName)
+        public static void CheckStringNullOrWhiteSpace(string value, string propertyName,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
         {
             if (string.IsNullOrWhiteSpace(value))
             {
@@ -29,7 +46,10 @@ namespace KingSurvival
             }
         }
 
-        public static void CheckCharIsNotLetter(char value, string propertyName)
+        public static void CheckCharIsNotLetter(char value, string propertyName,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
         {
             if (!char.IsLetter(value))
             {
@@ -37,11 +57,16 @@ namespace KingSurvival
             }
         }
 
-        private static void GetMethodClass(out String className, out String methodName)
+        private static void GetMethodClass(out String className, out String methodName,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
         {
             var stackTrace = new StackTrace();
             methodName = stackTrace.GetFrame(2).GetMethod().Name;
             className = stackTrace.GetFrame(2).GetType().Name;
         }
+
+       
     }
 }
