f063eed9300e35b08964d41bcbc77ead6fc9d35e
Martin <martin_n_marinov@mail.bg>
2014-07-14 21:35:16 +0300

    Implemented a TestConsole class in order to simulate user input for testing purposes.

---
+++ KingSurvival/UnitTestsKingSurvival/TestConsole.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace UnitTestsKingSurvival
+{
+    /// <summary>
+    /// This class redirects the standard input from the console to a 
+    /// StringReader. Every time the method Console.ReadLine() is called 
+    /// the input goes to a StringReader variable. This is done for testing purposes.
+    /// </summary>
+    public class TestConsole
+    {
+        private StringReader simulatedUserRead;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public TestConsole()
+        {
+
+        }
+
+        /// <summary>
+        /// This destructor closes the streams when all references to the class are gone!
+        /// </summary>
+        ~TestConsole()
+        {
+            if (this.simulatedUserRead != null)
+            {
+                this.simulatedUserRead.Close();
+            }
+        }
+
+        /// <summary>
+        /// This method redirects the result of the Console.ReadLine() function (and others.. but mainly that one)
+        /// and it now reads from simulatedUserRead !
+        /// </summary>
+        private void RedirectStandartInput()
+        {
+            // Redirect standard Console input.
+            Console.SetIn(simulatedUserRead);
+        }
+
+        /// <summary>
+        /// This method resets the ConsleIn to the standard stream.
+        /// </summary>
+        public void ResetInputAndOutputToStandard()
+        {
+            // Reset console in.
+            StreamReader standartIn = new StreamReader(Console.OpenStandardInput());
+            standartIn.Dispose();
+            Console.SetIn(standartIn);
+        }
+
+        /// <summary>
+        /// This method tests one line of input from the fake user!
+        /// When a Console.ReadLine() method is called the input gets flushed.
+        /// </summary>
+        /// <param name="input">The string we want to test with.</param>
+        public void testWrite(string input)
+        {
+            if (this.simulatedUserRead != null)
+            {
+                this.simulatedUserRead.Close();
+            }
+
+            this.simulatedUserRead = new StringReader(input);
+            RedirectStandartInput();
+        }
+    }
+}
---
+++ KingSurvival/UnitTestsKingSurvival/TestConsoleTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestsKingSurvival
+{
+    [TestClass]
+    public class TestConsoleTests
+    {
+        [TestMethod]
+        [TestCategory("FunctionalityTests")]
+        public void TestConsoleFunctionalityExample()
+        {
+            TestConsole testConsole = new TestConsole();
+
+            // Here the input gets redirected. (if the testWrite function is never used the input is never redirected).
+            testConsole.testWrite("12"); // Simulates a number typed by the user.
+            int number = int.Parse(Console.ReadLine());
+            Assert.AreEqual(12, number);
+
+            testConsole.testWrite("Pesho"); // Simulates a string typed by the user.
+            string name = Console.ReadLine();
+            Assert.AreEqual("Pesho", name);
+
+            // This redirects the input back to normal. Must be done 
+            //after every use of the TestConsole!
+            testConsole.ResetInputAndOutputToStandard(); 
+        }
+    }
+}
