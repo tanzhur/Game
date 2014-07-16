6253dc56fdb664e32c946472df3b4f01173e1c5f
fast4y <dzhenko@gmail.com>
2014-07-16 18:02:06 +0300

    Added proper GameLogic1Mover

---
+++ KingSurvival/KingSurvival/LogicPlayer1PieceMover.cs
@@ -0,0 +1,26 @@
+﻿namespace KingSurvival
+{
+    using System.Collections.Generic;
+
+    using Interfaces;
+
+    // strategy design pattern
+    public class LogicPlayer1PieceMover : LogicPlayerPieceMoverBase
+    {
+        public override IPiece FindPieceToMove(ICommand command, IList<IList<IPiece>> allPieces, out bool addScore)
+        {
+            addScore = false;
+
+            foreach (var piece in allPieces[0])
+            {
+                if (command.TargetID == piece.ID && piece.IsValidMove(command.Move))
+                {
+                    addScore = true;
+                    return piece;
+                }
+            }
+
+            return null;
+        }
+    }
+}
