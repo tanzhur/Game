2e269c135ba11f41758a448060151bcacce80f99
Martin <martin_n_marinov@mail.bg>
2014-07-22 20:13:27 +0300

    TextRendererDecoratorTests.cs class tested 100%.

---
+++ KingSurvival/UnitTestsKingSurvival/TextRendererDecoratorTests.cs
@@ -0,0 +1,37 @@
+﻿namespace UnitTestsKingSurvival
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using KingSurvival;
+    
+    [TestClass]
+    public class TextRendererDecoratorTests
+    {
+        static TestConsole testConsole = new TestConsole();
+        static TextRendererDecorator renderer;
+
+        [TestCleanup]
+        public void Cleanup() 
+        {
+            testConsole.ResetInputAndOutputToStandard();
+        }
+
+        [TestMethod]
+        [TestCategory("InitializationTests")]
+        public void TextRendererDecoratorInitializationTest() 
+        {
+            renderer = new TextRendererDecorator(new RendererConsole());
+        }
+
+        [TestMethod]
+        [TestCategory("FunctionalityTests")]
+        public void RenderTextMethodTest()
+        {
+            renderer = new TextRendererDecorator(new RendererConsole());
+            testConsole.RedirectStandartOutputToConsole();
+            renderer.RenderText("text", 2, 2);
+            string answer = testConsole.GetWrittenContent();
+            Assert.AreEqual("text", answer);
+        }
+    }
+}
