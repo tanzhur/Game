aa5e8ffcdb84ae8dfad0d78d44ef43492307f486
fast4y <dzhenko@gmail.com>
2014-08-01 12:05:16 +0300

    final final push

---
+++ KingSurvival/KingSurvival.Game/Common/GameValidator.cs
@@ -0,0 +1,36 @@
+﻿namespace KingSurvival.Game.Common
+{
+    using System;
+
+    /// <summary>
+    /// Static class used to validate all kinds of game data and throwing exceptions if needed.
+    /// </summary>
+    public static class GameValidator
+    {
+        /// <summary>
+        /// Checks if value is null.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <param name="propertyName">The property name to show if an exception is thrown.</param>
+        public static void CheckValueIsNull(object value, string propertyName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(propertyName + " can not be null.");
+            }
+        }
+
+        /// <summary>
+        /// Checks if value is not letter.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <param name="propertyName">The property name to show if an exception is thrown.</param>
+        public static void CheckCharIsNotLetter(char value, string propertyName)
+        {
+            if (!char.IsLetter(value))
+            {
+                throw new ArgumentException(propertyName + " must be letter.");
+            }
+        }
+    }
+}
---
+++ KingSurvival/KingSurvival.Game/Interfaces/IPiece.cs
@@ -0,0 +1,21 @@
+﻿namespace KingSurvival.Game.Interfaces
+{
+    using KingSurvival.Game.Common;
+    using KingSurvival.Game.Enums;
+
+    /// <summary>
+    /// Game object holding information about it's ID and position via Coordinates
+    /// </summary>
+    public interface IPiece : IMovableGamePiece, IObservableGamePiece
+    {
+        /// <summary>
+        /// Gets or sets the ID by which the IPiece is recognized among others.
+        /// </summary>
+        char ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the X-Y pair representing a point of the 2D space occupied by the IPiece.
+        /// </summary>
+        ICoordinates Coordinates { get; set; }
+    }
+}
